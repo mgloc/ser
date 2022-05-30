cb7652b948f9ae568f6a92f6d8521fe8cde687e7
gloxounet <mglock@outlook.fr>
2022-05-30 16:15:49 +0200

    changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ variables = gp.get_variables(inputData)
 
 # Attribution des variables
 nb_vertices = variables['nb_vertices']
-v_0 = variables['v_0']
+v_0 = variables['v_0'] #TODO MAYBE ERROR HERE##################################################
 
 c_fix    = variables['c_fix']
 c_var    = variables['c_var']
@@ -25,7 +25,7 @@ p_umd    = variables['p_umd']
 alpha    = variables['alpha']
 teta_fix = variables['teta_fix']
 teta_var = variables['teta_var']
-Tflh     = variables['Tflhc_fix']
+Tflh     = variables['Tflh']
 beta     = variables['beta']
 lbd      = variables['lbd']
 l        = variables['l']
@@ -34,15 +34,21 @@ D        = variables['D']
 C_max    = variables['C_max']
 Q_max    = variables['Q_max']
 
+print("################# VARIABLES ###################")
+for key,var in variables.items() :
+    print("---")
+    print(f"{key} : {var}")
+print("################# VARIABLES END ###############")
+
 L = [range(0,nb_vertices)]
 X = LpVariable('X', [L, L], cat='Binary')
-P_in = LpVariable('P_in', [L, L], lowbound = 0, cat='Continuous')
-P_out = LpVariable('P_out', [L, L], lowbound = 0, cat='Continuous')
+P_in = LpVariable('P_in', [L, L], cat='Continuous')
+P_out = LpVariable('P_out', [L, L], cat='Continuous')
 
 #------------------------------CONTRAINTES & PROBLEME------------------------------
 
 Heating_Energy_Network_Optimization_Problem = LpProblem("Heating_Energy_Network_Optimization_Problem", LpMinimize)
-Heating_Energy_Network_Optimization_Problem += total_expense(c_rev, D, lbd, X, Tflh, c_heat, beta, P_in, c_om, l, p_umd, c_fix, alpha, c_var, nb_vertices)
+Heating_Energy_Network_Optimization_Problem += total_expense(c_rev, D, lbd, X, Tflh, c_heat, beta, P_in, c_om, l, p_umd, c_fix, alpha, c_var, nb_vertices,v_0)
 Heating_Energy_Network_Optimization_Problem += contrainte_1(X, nb_vertices)
 Heating_Energy_Network_Optimization_Problem += contrainte_2(X, nb_vertices)
 Heating_Energy_Network_Optimization_Problem += contrainte_3(teta_var, teta_fix, l, lbd, beta, d, P_in, P_out, X, nb_vertices)
@@ -51,6 +57,9 @@ Heating_Energy_Network_Optimization_Problem += contrainte_5(P_in, X, C_max, nb_v
 Heating_Energy_Network_Optimization_Problem += contrainte_6(X, v_0, nb_vertices)
 Heating_Energy_Network_Optimization_Problem += contrainte_7(P_in, Q_max, v_0, nb_vertices)
 Heating_Energy_Network_Optimization_Problem += contrainte_8(X, v_0, nb_vertices)
+
+#------------------------------RESOLUTION-----------------------------------------
+
 Heating_Energy_Network_Optimization_Problem.solve()
 
 
--- scripts/get_parameters.py
+++ scripts/get_parameters.py
@@ -26,7 +26,7 @@ variables = {
 
 def read_excel_data(filename:str, sheet_name:str):
     """Function use to read excel data"""
-    data = pd.read_excel(filename, sheet_name=sheet_name, header=None, engine='openpyxl')
+    data = pd.read_excel(filename, sheet_name=sheet_name, header=None)
     values = data.values
     if min(values.shape) == 1:  # This If is to make the code insensitive to column-wise or row-wise expression #
         if values.shape[0] == 1:
@@ -37,7 +37,7 @@ def read_excel_data(filename:str, sheet_name:str):
         return values[0]
     else:
         data_dict = {}
-        if min(values.shape) == 2:  # For single-dimension parameters in Excel
+        """if min(values.shape) == 2:  # For single-dimension parameters in Excel
             if values.shape[0] == 2:
                 for i in range(values.shape[1]):
                     data_dict[i+1] = values[1][i]
@@ -45,10 +45,10 @@ def read_excel_data(filename:str, sheet_name:str):
                 for i in range(values.shape[0]):
                     data_dict[i+1] = values[i][1]
 
-        else:  # For two-dimension (matrix) parameters in Excel
-            for i in range(values.shape[0]):
-                for j in range(values.shape[1]):
-                    data_dict[(i+1, j+1)] = values[i][j]
+        else:  # For two-dimension (matrix) parameters in Excel"""
+        for i in range(values.shape[0]):
+            for j in range(values.shape[1]):
+                data_dict[(i, j)] = values[i][j]
         return data_dict
 
 def get_variables(inputData:str)->dict :
@@ -63,7 +63,7 @@ def get_variables(inputData:str)->dict :
     l = {}
     for i in range(variables['nb_vertices']):
         for j in range(variables['nb_vertices']):
-            l[(i,j)] = np.sqrt((node_coord(i,1)-node_coord(j,1))**2 + (node_coord(i,2)-node_coord(j,2))**2)
+            l[(i,j)] = np.sqrt((node_coord[(i,0)]-node_coord[(j,0)])**2 + (node_coord[(i,1)]-node_coord[(j,1)])**2)
     variables['l'] = l
 
     variables['teta_fix'] = read_excel_data(inputData, "vfix(thetaijfix)")
