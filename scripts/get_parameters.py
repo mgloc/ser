db56ad0c335b17a38cfc83c859c579270e5256e9
Para_Noah <nomi8@orange.fr>
2022-05-30 15:31:30 +0200

    get parameters

--- scripts/get_parameters.py
+++ scripts/get_parameters.py
@@ -1,5 +1,28 @@
 import pandas as pd
 
+#Paramètres
+variables = {
+    'v_0':0,
+    'nb_vertices':0,
+    'c_fix' :0,
+    'c_var' :0,
+    'c_om' :0,
+    'c_heat' :0,
+    'c_rev' :0,
+    'p_umd' :0,
+    'alpha' :0,
+    'teta_fix' :0,
+    'teta_var' :0,
+    'Tflh':0,
+    'beta' :0,
+    'lbd' :0,
+    'l' :0,
+    'd':0, 
+    'D' :0,
+    'C_max' :0, 
+    'Q_max' :0
+    }
+
 # Input Data Preparation #
 def read_excel_data(filename, sheet_name):
     data = pd.read_excel(filename, sheet_name=sheet_name, header=None, engine='openpyxl')
@@ -44,25 +67,11 @@ def testRead(inputData) :
     array2 = read_excel_data(inputData, "array2")
     print("array2: ", array2)
 
-#Paramètres
-variables = {
-    'v_0':0,
-    'nb_vertices':0,
-    'c_fix' :0,
-    'c_var' :0,
-    'c_om' :0,
-    'c_heat' :0,
-    'c_rev' :0,
-    'p_umd' :0,
-    'alpha' :0,
-    'teta_fix' :0,
-    'teta_var' :0,
-    'Tflh':0,
-    'beta' :0,
-    'lbd' :0,
-    'l' :0,
-    'd':0, 
-    'D' :0,
-    'C_max' :0, 
-    'Q_max' :0
-    }
+    v_0 = read_excel_data(inputData, "SourceNum")
+    variables['v_0'] = v_0[0]
+
+    nb_vertices = read_excel_data(inputData, "Nodes")
+    nb_vertices = nb_vertices[0]
+
+    node_coord = read_excel_data(inputData,"NodesCord")
+
